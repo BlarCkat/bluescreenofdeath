5684ce5f5fe6982980680519d616de3073e6ff53
John St. Aïmond Banson <articwolf177@gmail.com>
2024-07-21 20:31:18 -0700

    first_update

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter, Inter_Tight } from "next/font/google";
+import "./globals.css";
+
+const interTight = Inter_Tight({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Blue Screen of Death",
+  description: "A few lines of code in software update could affect millions worldwide. Code responsibly or you could be responsible for one the biggest outages in history.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={interTight.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+const HomeView = () => {
+  return ( 
+    <>
+    <main className="h-screen w-screen bg-[#0076E0] px-[10%] py-[5vh]">
+      <div className="w-full"><p className="lg:text-[120px]">&#58;&#40;</p></div>
+      <div className="text-white"><p className="lg:text-[60px] leading-tight">It&apos;s not your PC, someone just pushed code to production on a Friday.</p></div>
+      <div className="text-white"><p className="lg:text-[40px] leading-tight">85% complete</p></div>
+    </main>
+    </>
+   );
+}
+ 
+export default HomeView;
